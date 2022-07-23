edd4ac18a90952f3a2e24352525800079c242c06
Angelina Medved <ang.medved@yandex.by>
2022-07-23 22:24:51 +0300

    added local storage

--- my-cinema/src/components/BasicModal/index.jsx
+++ my-cinema/src/components/BasicModal/index.jsx
@@ -37,13 +37,19 @@ export default function BasicModal({isOpen, setIsOpen}) {
 
   const [phone, setPhone] = useState('');
 
-  const [modalState, setModalState] = useState(true);
+  const [formType, setIsFormType] = useState(true);
 
   const [errorEmail, setIsEmailError] = useState(false);
 
   const [isEqualPasswords, setIsEqualPasswords] = useState(true);
 
   const [passwordShown, setPasswordShown] = useState(false);
+
+  const [isAuth, setIsAuth] = useState(false);
+
+  const [isUserAuthrized, setIsUserAuthrized] = useState(true);
+
+  const [isUserAuthorized, setIsUserAuthorized] = useState(false);
 
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value);
@@ -58,7 +64,11 @@ export default function BasicModal({isOpen, setIsOpen}) {
   }
 
   const handleSignUp = () => {
-    setModalState(false);
+    setIsFormType(false);
+  }
+
+  const handlSignIn = () => {
+    setIsFormType(true);
   }
 
   const handleEmailChange = (e) => {
@@ -79,72 +89,160 @@ export default function BasicModal({isOpen, setIsOpen}) {
     return re.test(email);
   };
 
-  const handleSubmit = () => {
-    console.log('email', email);
-    console.log('firsName', firstName)
-    console.log('lastName', lastName);
-    console.log('phone', phone);
-
-    if(password === repeatPassword) {
-      setIsEqualPasswords(true);
-    } else {
-      setIsEqualPasswords(false);
-    }
-
-    if(email !== isValidEmail) {
-      setIsEmailError(true)
-    }
-  }
-
   const handleShowPassword = () => {
     setPasswordShown(!passwordShown);
   }
 
+  const usersJson = localStorage.getItem('users');
+
+  const users = JSON.parse(usersJson) || [];
+
+  const newUser = {
+    isAuth: false,
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+    phone: phone,
+  }
+
+  const [isExistEmail, setIsEmailExist] = useState(false);
+  
+  const checkIsEmailExist = users.find(i => email === i.email);
+
+    const handleSubmit = () => {
+
+      if(password === repeatPassword) {
+        setIsEqualPasswords(true);
+      } 
+
+      if((!checkIsEmailExist) && (repeatPassword === password)) {
+        setIsEmailExist(true);
+        localStorage.setItem('users', JSON.stringify([...users, newUser]));
+      } 
+    }
+    if(email !== isValidEmail) {
+      setIsEmailError(true)
+    } else {
+      setIsEmailError(false);
+    }
+  
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
+
       <Modal
         open={isOpen}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        {modalState
+
+        {formType
+
         ?
+
         <Box className="container" sx={style}>
-        <Typography className="modal-title" variant="h6" component="h2">
-          Sign In
+        <Typography 
+        className="modal-title"
+        variant="h6"
+        component="h2">
+        Sign In
         </Typography>
-        <TextField id="standard-basic" label="Email" type='email' variant="standard" value={email} onChange={handleEmailChange}/>
-        <TextField label="Password" type={passwordShown ? "text" : "password"} variant="standard"  value={password} onChange={handleChangePassword}/>
+
+        <TextField 
+        id="standard-basic" 
+        label="Email" 
+        type='email' 
+        variant="standard" 
+        value={email} 
+        onChange={handleEmailChange}/>
+
+        <TextField 
+        label="Password" 
+        type={passwordShown ? "text" : "password"} 
+        variant="standard"  
+        value={password} 
+        onChange={handleChangePassword}/>
+
         <RemoveRedEyeIcon className='showPswSgnIn' onClick={handleShowPassword}/>
         
-        <Button disabled={(!email, !password)} className='sign-in-btn' href="#text-buttons">Sign In</Button>
+        <Button disabled={(!email, !password)} 
+        className='sign-in-btn' 
+        href="#text-buttons">Sign In</Button>
+
         <div>
           Don't you have an account? <Button onClick={handleSignUp}>Create one</Button>  
         </div>
       </Box>
+
       :
+
       <Box className="container" sx={style}>
-        <Typography className="modal-title-sign-up" variant="h6" component="h2">
-          Sign Up</Typography>
-        <TextField id="standard-basic" type='email' label="email" variant="standard" value={email} onChange={handleEmailChange}/>
+        <Typography 
+        className="modal-title-sign-up" 
+        variant="h6" component="h2">
+        Sign Up</Typography>
+
+        <TextField 
+        id="standard-basic" 
+        type='email' 
+        label="email" 
+        variant="standard" 
+        value={email} 
+        onChange={handleEmailChange}/>
+
         {errorEmail && <div className='error-message'>Email is not valid</div>}
-        <TextField id="standard-basic" label="First name" variant="standard" value={firstName} onChange={handleFirstNameChange}/>
-        <TextField id="standard-basic" label="Last name" variant="standard" value={lastName} onChange={handleLastNameChange}/>
-        <TextField id="standard-basic" type="tel" label="phone" variant="standard" value={phone} onChange={handlePhoneChange}/>
+
+        <TextField 
+        id="standard-basic" 
+        label="First name" 
+        variant="standard" 
+        value={firstName} 
+        onChange={handleFirstNameChange}/>
+
+        <TextField 
+        id="standard-basic" 
+        label="Last name" 
+        variant="standard" 
+        value={lastName} 
+        onChange={handleLastNameChange}/>
+
+        <TextField id="standard-basic" 
+        type="tel" 
+        label="phone" 
+        variant="standard" 
+        value={phone} 
+        onChange={handlePhoneChange}/>
        
-        <TextField label="Password" type={passwordShown ? "text" : "password"} variant="standard" value={password} onChange={handleChangePassword}/>
+        <TextField label="Password" 
+        type={passwordShown ? "text" : "password"} 
+        variant="standard" 
+        value={password} 
+        onChange={handleChangePassword}/>
+
         <RemoveRedEyeIcon className='showPswSgnUp' onClick={handleShowPassword}/>
   
-        <TextField label="Repeat password" type={passwordShown ? "text" : "password"} variant="standard" value={repeatPassword} onChange={handleRepeatPassword} />
+        <TextField label="Repeat password" 
+        type={passwordShown ? "text" : "password"} 
+        variant="standard" 
+        value={repeatPassword} 
+        onChange={handleRepeatPassword} />
+
         <RemoveRedEyeIcon className='showPswSgnUpRep' onClick={handleShowPassword}/>
 
         {!isEqualPasswords && <div className='error-message'>Passwords are not equal</div>}
-
-        <Button disabled={(!email, !firstName, !lastName, !phone, !password)} onClick={handleSubmit} className='sign-in-btn' href="#text-buttons">Sign Up</Button>
+        {isAuth && <div className='error-message'>You're already have an account</div>}
+        <Button 
+        disabled={(!email, !firstName, !lastName, !phone, !password)} 
+        onClick={handleSubmit} 
+        href="#text-buttons">Sign Up</Button>
+        <Button onClick={handlSignIn}> go to sign in
+        </Button>
+      
       </Box>
       }
+
       </Modal>
     </div>
   );
